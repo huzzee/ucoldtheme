5303d87090637027354dfa6584c24e9e62b527aa
laraib610 <laraibkhan8242@gmail.com>
2025-10-06 13:58:15 +0500

    register modals

--- umrahcompanions.com/assets/frontend/js/common.js
+++ umrahcompanions.com/assets/frontend/js/common.js
@@ -269,42 +269,42 @@
                     $('.modal select').formSelect();
                     
                     /* Phone 3 */
-                        var input = document.querySelector("#phone3");
-                        var instance = window.intlTelInput(input, {
-                            separateDialCode: true,
-                            initialCountry: "auto",
-                            geoIpLookup: function(success, failure) {
-                                $.get("https://ipinfo.io/", function() {}, "jsonp").always(function(resp) {
-                                    var countryCode = (resp && resp.country) ? resp.country : "us";
-                                    success(countryCode);
-                                });
-                            },
-                        });
-                        var countryData = instance.getSelectedCountryData();
-                        $('.country_code').val(countryData.dialCode);
-                        input.addEventListener("countrychange",function() {
-                            var countryData = instance.getSelectedCountryData();
-                            $('.country_code').val(countryData.dialCode);
-                        });
+                        // var input = document.querySelector("#userPhone");
+                        // var instance = window.intlTelInput(input, {
+                        //     separateDialCode: true,
+                        //     initialCountry: "auto",
+                        //     geoIpLookup: function(success, failure) {
+                        //         $.get("https://ipinfo.io/", function() {}, "jsonp").always(function(resp) {
+                        //             var countryCode = (resp && resp.country) ? resp.country : "us";
+                        //             success(countryCode);
+                        //         });
+                        //     },
+                        // });
+                        // var countryData = instance.getSelectedCountryData();
+                        // $('.country_code').val(countryData.dialCode);
+                        // input.addEventListener("countrychange",function() {
+                        //     var countryData = instance.getSelectedCountryData();
+                        //     $('.country_code').val(countryData.dialCode);
+                        // });
                     /* Phone 3 */
 
-                    var input = document.querySelector("#phone4");
-                    var instance = window.intlTelInput(input, {
-                        separateDialCode: true,
-                        initialCountry: "auto",
-                        geoIpLookup: function(success, failure) {
-                            $.get("https://ipinfo.io/", function() {}, "jsonp").always(function(resp) {
-                                var countryCode = (resp && resp.country) ? resp.country : "us";
-                                success(countryCode);
-                            });
-                        },
-                    });
-                    var countryData = instance.getSelectedCountryData();
-                    $('.country_code').val(countryData.dialCode);
-                    input.addEventListener("countrychange",function() {
-                        var countryData = instance.getSelectedCountryData();
-                        $('.country_code').val(countryData.dialCode);
-                    });
+                    // var input = document.querySelector("#phone4");
+                    // var instance = window.intlTelInput(input, {
+                    //     separateDialCode: true,
+                    //     initialCountry: "auto",
+                    //     geoIpLookup: function(success, failure) {
+                    //         $.get("https://ipinfo.io/", function() {}, "jsonp").always(function(resp) {
+                    //             var countryCode = (resp && resp.country) ? resp.country : "us";
+                    //             success(countryCode);
+                    //         });
+                    //     },
+                    // });
+                    // var countryData = instance.getSelectedCountryData();
+                    // $('.country_code').val(countryData.dialCode);
+                    // input.addEventListener("countrychange",function() {
+                    //     var countryData = instance.getSelectedCountryData();
+                    //     $('.country_code').val(countryData.dialCode);
+                    // });
                      
                      
 
